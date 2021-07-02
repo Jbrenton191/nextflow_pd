7a485ef42ef342b2a729d60b442846a35ed7bcdd
Jbrenton191 <jwbrenton@gmail.com>
2021-07-02 16:52:57 +0100

    added leafcutter.nf and list of files and also some scripts

--- convert_STAR_SJ_to_junc.R
+++ convert_STAR_SJ_to_junc.R
@@ -3,16 +3,20 @@ library(tidyverse)
 
 arguments <- parse_args(OptionParser(), positional_arguments = 3)
 
-sj_dir_paths <- arguments$args[1] %>% str_split(",") %>% unlist()
+sj_dir_path <- arguments$args[1] %>% str_split(",") %>% unlist()
 base_dir<- arguments$args[2]
 output_path <- arguments$args[3]
 
-RNAseqProcessing::convert_STAR_SJ_to_junc(sj_dir_path, base_dir, 
-filter_out_blacklist_regions=TRUE, path_to_ENCODE_blacklist = base_dir)
+# sj_dir_path <- "/home/jbrenton/nextflow_test/output/STAR/align"
+# base_dir<- "/home/jbrenton/nextflow_test"
+# output_path <- "/home/jbrenton/nextflow_test/output/leafcutter"
+
+
+RNAseqProcessing::convert_STAR_SJ_to_junc(sj_dir_path, output_path, filter_out_blacklist_regions=F, path_to_ENCODE_blacklist = base_dir)
 
 junc_df <- tibble(junc_file_name = list.files(path = output_path,
                                               pattern = "_SJ_leafcutter.junc", full.names = TRUE))
  
 write_delim(junc_df,
-             path = str_c(output_path, "/list_juncfiles.txt"),
+             path = str_c(".", "/list_juncfiles.txt"),
              delim = "\t", col_names = F)
