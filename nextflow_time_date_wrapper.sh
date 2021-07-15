512629c60b4dca0bf36a783cf6496589d9319424
Jbrenton191 <jwbrenton@gmail.com>
2021-07-15 23:03:13 +0100

    added from ryten server

---
+++ nextflow_time_date_wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Add the time and date onto reports and timeline names
+
+nf_pipeline=$1
+
+## Get current date ##
+_date=$(date +"%m_%d_%Y")
+_time=$(date +"%H:%M:%S")
+## Appending a current date from a $_now to a filename stored in $_file ##
+timing="${_date}-${_time}"
+
+## Run nextflow in background with dated timeline and report ##
+nextflow run $nf_pipeline -bg -with-timeline ${nf_pipeline}_timeline_${timing}.html -with-report ${nf_pipeline}_report_${timing}.html 
